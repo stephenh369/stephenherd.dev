b575589f7dd1d80c3a605dab9a8604c05d0443bc
stephenh369 <stephen_h369@live.co.uk>
2021-08-17 14:41:06 +0100

    made Typewriter component, refactoring Hero in process

--- components/Hero.js
+++ components/Hero.js
@@ -1,48 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import Typewriter from './Typewriter'
 
 export default function Hero() {
- const [typing, setTyping] = useState('')
- const [words, setWords] = useState(['using custom code.', 'with WordPress.', 'that provide E-Commerce solutions.'])
- const [index, setIndex] = useState(0)
- const [letterIndex, setLetterIndex] = useState(0)
-
- const handleClear = () => {
-    setLetterIndex(0)
-    if(index === words.length -1) {
-      setIndex(0)
-      setTyping(typing => typing = '')
-    } else setIndex(index + 1)
- }
-
- const handleTyping = () => {
-  if(letterIndex < words[index].length) {
-    setTimeout(() => {
-      if (letterIndex === 0) {
-        setTyping(prevState => words[index].charAt(letterIndex))
-      } else {
-        setTyping(prevState => [...prevState, words[index].charAt(letterIndex)])
-      }
-      setLetterIndex(letterIndex + 1)
-    }, 200)
-  } else {
-    setTimeout(() => {
-      handleClear()
-    }, 2000)
-  } 
-}
-
- useEffect(() => {
-   handleTyping()
-}, [index, letterIndex]);
-
 
   return (
     <div className="bg-hero bg-cover bg-center bg-no-repeat bg-blue-900 bg-opacity-50 bg-blend-overlay h-screen w-full flex items-center">
       <div className="mx-8 md:mx-16 p-4 rounded shadow-lg bg-gray-900 bg-opacity-50">
         <h1 className="pt-2 pb-20 xs:pb-4">Stephen Herd - Web Developer</h1>
-        <div className="h-20 xs:h-8 sm:h-10 md:h-full">
-          <h3 className="pb-4">I can build websites <span className="text-blue-400">{typing}</span><span className="animate-cursor text-blue-400">|</span></h3>
-        </div>
+        <Typewriter/>
       </div>
     </div>
   )
---
+++ components/Typewriter.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react'
+
+export default function Typewriter() {
+  const [typing, setTyping] = useState('')
+  const [words, setWords] = useState(['using custom code.', 'with WordPress.', 'that provide E-Commerce solutions.'])
+  const [index, setIndex] = useState(0)
+  const [letterIndex, setLetterIndex] = useState(0)
+ 
+  const handleClear = () => {
+     setLetterIndex(0)
+     if(index === words.length -1) {
+       setIndex(0)
+       setTyping(typing => typing = '')
+     } else setIndex(index + 1)
+  }
+ 
+  const handleTyping = () => {
+   if(letterIndex < words[index].length) {
+     setTimeout(() => {
+       if (letterIndex === 0) {
+         setTyping(prevState => words[index].charAt(letterIndex))
+       } else {
+         setTyping(prevState => [...prevState, words[index].charAt(letterIndex)])
+       }
+       setLetterIndex(letterIndex + 1)
+     }, 200)
+   } else {
+     setTimeout(() => {
+       handleClear()
+     }, 2000)
+   } 
+ }
+ 
+  useEffect(() => {
+    handleTyping()
+ }, [index, letterIndex]);
+
+  return (
+    <div className="h-20 xs:h-8 sm:h-10 md:h-full">
+      <h3 className="pb-4">I can build websites <span className="text-blue-400">{typing}</span><span className="animate-cursor text-blue-400">|</span></h3>
+    </div>
+  )
+}
