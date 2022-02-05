9edf52c79ba96d2fd9f2a4325ff392ab05e87b37
stephenh369 <stephen_h369@live.co.uk>
2022-02-05 12:21:23 +0000

    header background on scroll

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,25 @@
 import Nav from './Nav'
 import NavMobile from './NavMobile'
+import { useState, useEffect } from 'react'
 
 export default function Header() {
+
+  const [scrollY, setScrollY] = useState(0)
+
+  useEffect(() => {
+    const options = { passive: true }
+    const scroll = () => {
+     const { pageYOffset } = window
+      setScrollY(pageYOffset)
+    }
+    document.addEventListener("scroll", scroll, options);
+    () => document.removeEventListener("scroll", scroll, options)
+   }, [])
+
+
   return (
     <>
-    <header className="fixed top-0 left-0 z-50 w-full flex items-center justify-between py-2 px-10 md:px-20">
+    <header className={`${scrollY > 300 ? "bg-gray-900 + bg-opacity-50" : ""} fixed top-0 left-0 z-50 w-full flex items-center justify-between py-2 px-10 md:px-20`}>
       <a className="p-0" href="#main"><h1 className="py-2"><span className="text-blue-400">sh</span>.dev</h1></a>
       <Nav/>
     </header>
