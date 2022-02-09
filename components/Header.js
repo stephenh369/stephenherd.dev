084087af532371ddf2e02dee4d0ab1b8faf82a86
stephenh369 <stephen_h369@live.co.uk>
2022-02-09 21:53:55 +0000

    success page adjustments and back button, header logo now returns to root

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Nav from './Nav'
 import NavMobile from './NavMobile'
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 export default function Header() {
 
@@ -20,7 +21,7 @@ export default function Header() {
   return (
     <>
     <header className={`${scrollY > 300 ? "bg-gray-900 + bg-opacity-50" : ""} fixed top-0 left-0 z-50 w-full flex items-center justify-between py-2 px-10 md:px-20`}>
-      <a className="p-0" href="#main"><h1 className="py-2"><span className="text-blue-400">sh</span>.dev</h1></a>
+      <Link href="/"><a className="p-0"><h1 className="py-2"><span className="text-blue-400">sh</span>.dev</h1></a></Link>
       <Nav/>
     </header>
     <header className="md:hidden w-full fixed top-24 right-0 z-50 text-right">
--- pages/success.js
+++ pages/success.js
@@ -1,12 +1,15 @@
+import Link from "next/link"
+
 export default function Success() {
   return (
-    <div id="success" className="h-100 px-10 py-20 md:p-20 bg-indigo-700">
-      <div className="w-full h-full">
+    <div id="success" className="h-screen px-10 py-20 md:p-20 bg-indigo-700">
+      <div className="w-full h-full flex flex-col justify-center items-center">
         <div className="flex flex-col text-center justify-center pb-8">
           <h1>Success</h1>
         </div>
         <div className="flex flex-col text-center justify-center pt-8">
-          <p>Your message has been sent! I will get back to you as soon as possible.</p>
+          <p className="pb-8">Your message has been sent! I will get back to you as soon as possible.</p>
+          <Link href="/"><a className="border-2 border-blue-400 hover:text-blue-400 nav-btn">Back</a></Link>
         </div>
       </div>
     </div>
